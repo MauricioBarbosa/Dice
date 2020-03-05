3e45a7e625378f62b493827fac0b4fd2464cbd92
mauriciobarboza6.1@gmail.com <mauriciobarboza6.1@gmail.com>
2020-03-05 13:43:47 -0300

    Adicionar arquivos de projeto.

---
+++ LP4Project/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using LP4Project.Models;
+
+namespace LP4Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Produto[] prods=new Produto[12];
+            for(int i = 0; i < 12; i++)
+            {
+                prods[i] = new Produto(i, "Produto", 12.00, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dictum nulla vitae bibendum ullamcorper");
+            }
+
+            ViewBag.Produtos = prods;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LP4Project/Models/Produto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LP4Project.Models
+{
+    public class Produto
+    {
+        private int idProduto;
+        private String nomeProduto;
+        private double precoProduto;
+        private String descricaoProduto;
+
+        public Produto(int idProduto, string nomeProduto, double precoProduto,string descricaoProduto)
+        {
+            this.idProduto = idProduto;
+            this.nomeProduto = nomeProduto;
+            this.precoProduto = precoProduto;
+            this.DescricaoProduto = descricaoProduto;
+        }
+
+        public double PrecoProduto { get => precoProduto; set => precoProduto = value; }
+        public string NomeProduto { get => nomeProduto; set => nomeProduto = value; }
+        public int IdProduto { get => idProduto; set => idProduto = value; }
+        public string DescricaoProduto { get => descricaoProduto; set => descricaoProduto = value; }
+    }
+}
